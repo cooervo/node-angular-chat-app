22d1bf0838aa460b06a70d3ea4ddbfcd15ba954a
mateo <cuervo.taylor@gmail.com>
2016-03-29 16:53:26 -0500

    different usernames working

--- client/chat/chat.controller.js
+++ client/chat/chat.controller.js
@@ -11,17 +11,13 @@ angular.module("app")
 
 
             ctrl.submit = function () {
-
                 ctrl.socketIo.emit("chatMsg", {name: userService.name, msg: ctrl.inputMessage});
-
                 ctrl.inputMessage = null;
             };
 
 
             ctrl.socketIo.on("updateClients", function (userMsg) {
-
                 ctrl.messages.push(userMsg);
-
                 //apply changes to angular
                 $scope.$apply();
             });
